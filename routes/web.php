57090c1fdcbf2bfe59a355349a40688644479566
guiviveiro@gmail.com <guiviveiro@gmail.com>
2025-03-24 22:05:02 -0300

    Nesse commit, aprendemos como encaminhar os parametros atráves das rotas pelo framework laravel

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,15 @@ use App\Http\Controllers\WelcomeController;
 // Route::get('/', [WelcomeController::class, 'show']);
 Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal']);
 Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos']);
+
 Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato']);
+// nome, categoria, assunto, mensagem
+
+Route::get(
+    '/contato/{nome}/{categoria}/{assunto}/{mensagem}', 
+    function(string $nome, string $categoria, string $assunto, $mensagem) {
+    echo "Estamos aqui: $nome - $categoria - $assunto - $mensagem";
+});
 
 /* Verbo Http
 
