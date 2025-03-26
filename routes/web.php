da78fa20c33eaea601614ff7d14ea860a589be9f
guiviveiro@gmail.com <guiviveiro@gmail.com>
2025-03-26 18:28:51 -0300

    Implementação das rotas login, clientes, fornecedores, produtos

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,11 @@ Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobr
 
 Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato']);
 
+Route::get('/login', function(){return 'Login';});
+Route::get('/clientes', function(){return 'Cliente';});
+Route::get('/fornecedores', function(){return 'Fornecedores';});
+Route::get('/produtos', function(){return 'Produtos';});
+
 /* Verbo Http
 
 get
