fdb057641e404c5980a0c18cb2a3dddeaac56559
guiviveiro@gmail.com <guiviveiro@gmail.com>
2025-03-31 22:56:55 -0300

    Nomeando as rotas para que não haja dependencia absoluta nas raizes das rotas

--- routes/web.php
+++ routes/web.php
@@ -14,16 +14,16 @@ use App\Http\Controllers\WelcomeController;
 */
 
 // Route::get('/', [WelcomeController::class, 'show']);
-Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal']);
-Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos']);
-Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato']);
-Route::get('/login', function(){return 'Login';});
+Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal'])->name('site.index');
+Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
+Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato'])->name('site.contato');
+Route::get('/login', function(){return 'Login';})->name('site.login');
 
 //app
 Route::prefix('/app')->group(function(){
-    Route::get('/clientes', function(){return 'Cliente';});
-    Route::get('/fornecedores', function(){return 'Fornecedores';});
-    Route::get('/produtos', function(){return 'Produtos';});
+    Route::get('/clientes', function(){return 'Cliente';})->name('app.clientes');
+    Route::get('/fornecedores', function(){return 'Fornecedores';})->name('app.fornecedores');
+    Route::get('/produtos', function(){return 'Produtos';})->name('app.produtos');
 });
 
 /* Verbo Http
