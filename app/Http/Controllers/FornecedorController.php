16131d633a30f921a6f98c43454c06ee2113f0d0
guiviveiro@gmail.com <guiviveiro@gmail.com>
2025-04-08 23:52:11 -0300

    Aqui vemos o operador condicional ternário, ele é mais chique do que a condição IF e diminui bastante o codigo

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -19,6 +19,12 @@ class FornecedorController extends Controller
             'status' => 'S', 
             ]
         ];
+
+        // condicao ? se verdadeiro : se falso;
+        // condicao ? se verdadeiro : (condicao ? se verdadeiro : se falso);
+        $msg = isset($fornecedores[0]['cnpj']) ? 'CNPJ informado' : 'CNPJ não informado';
+        echo $msg;
+
         return view('app.fornecedor.index', compact('fornecedores'));
     }
 }
