5b78c552fa4f6be73197b5d735b3de2754ee663a
guiviveiro@gmail.com <guiviveiro@gmail.com>
2025-04-10 22:39:17 -0300

    O switch case no PHP é uma estrutura de controle de fluxo que permite executar diferentes blocos de código com base no valor de uma variável

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -12,12 +12,23 @@ class FornecedorController extends Controller
             0 => [
             'nome' => 'Fornecedor 1', 
             'status' => 'N', 
-            'cnpj' => '0'
+            'cnpj' => '0',
+            'ddd' => '', // São Paul (SP)
+            'telefone' => '0000-0000'
         ],
-            1 => [
-                'nome' => 'Fornecedor 2', 
-                'status' => 'S', 
-                'cnpj' => "0"
+        1 => [
+            'nome' => 'Fornecedor 2', 
+            'status' => 'S', 
+            'cnpj' => null,
+            'ddd' => '85', //Fortaleza (CE)
+            'telefone' => '0000-0000'
+        ],
+        2 => [
+            'nome' => 'Fornecedor 2', 
+            'status' => 'S', 
+            'cnpj' => null,
+            'ddd' => '32', // Juiz d efora (MG)
+            'telefone' => '0000-0000'
             ]
         ];
         return view('app.fornecedor.index', compact('fornecedores'));
